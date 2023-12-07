6e50886a393f0b010077b588383cbe57f7d5c338
miocast <rinatsoi05@gmail.com>
2023-12-07 18:26:34 +0300

    Добавьте файлы проекта.

---
+++ FormGP.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace GraphicPrimitives
+{
+    partial class FormGP
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGP));
+            this.SuspendLayout();
+            // 
+            // FormGP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormGP";
+            this.Text = "Графические примитивы";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormGP_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ FormGP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicPrimitives
+{
+    public partial class FormGP : Form
+    {
+
+        public FormGP()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGP_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+
+    abstract class Shape
+    {
+        protected Brush FillColor { get; set; }
+        protected Pen BorderColor { get; set; }
+        protected int Width { get; set; }
+        protected int Height { get; set; }
+
+        public abstract void Draw(Graphics graphics);
+    }
+
+}
